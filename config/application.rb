862191ff7dac9c0c1c1b3ded74d0e6ab23ecb75e
Simon <simon.nizov@gmail.com>
2017-06-20 13:17:08 +0300

    rename serve static config option

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,7 @@ module Prelaunchr
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.serve_static_assets = true
+    config.serve_static_files = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
